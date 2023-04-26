678f9102e18e4e945c5c0fcc809f96fe603f8dfa
Gopal Adhikari <gopuadks@gmail.com>
2023-04-26 13:42:13 +0545

    fixed urls

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
       <TitleAndScrollRestoration />
       <Navbar />
       <Routes>
-        <Route path="/technical-backer-vite" element={<Home />} />
+        <Route path="/technical-backer-vite/" element={<Home />} />
         <Route path="/technical-backer-vite/services" element={<Services />} />
         <Route path="/technical-backer-vite/contact" element={<GetInTouch />} />
         <Route path="/technical-backer-vite/about" element={<About />} />
